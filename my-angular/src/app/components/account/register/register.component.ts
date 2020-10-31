677af72d53a32aafa030e6376a748dded5de6948
Volodymyr Novak <novakvova@gmail.com>
2020-10-31 13:23:39 +0200

    Good app server and client

--- my-angular/src/app/components/account/register/register.component.ts
+++ my-angular/src/app/components/account/register/register.component.ts
@@ -1,16 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Constants } from 'src/app/constants';
+
+interface RegisterModel {
+  email: string;
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
+  model: RegisterModel;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
 
@@ -20,7 +29,21 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log("Відправка форми");
+    console.log("Відправка форми", this.registerForm.controls.email.value);
+    this.model = {
+      email: this.registerForm.controls.email.value
+    };
+    this.http.post(`${Constants.HOME_URL}/api/Account/register`, this.model)
+      .subscribe(data=>{
+        console.log("Good requst", data);
+      },
+      badRequest => {
+        console.log("Error");
+      });
+
+    //Constants.HOME_URL
+
+    //;
   }
 
 }
